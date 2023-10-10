231dec27d9a01a50da4f6b697584628faf05c035
Kateryna.Roshchupkina <k.roshchup@gmail.com>
2023-10-10 13:59:46 +0100

    In order not to copy your solution, I tried to create my own :)

--- HW6_1.py
+++ HW6_1.py
@@ -5,8 +5,16 @@ details = [person('Kateryna', 37), person('Dmytro', 40), person('Nadia', 17)]
 
 expected_datatypes = (str, int)
 
-for person in details:
-    if isinstance(person.Name, expected_datatypes) and isinstance(person.Age, expected_datatypes):
-        print(f'Name: {person.Name}, Age: {person.Age}')
+for p in details:
+    name_valid = isinstance(p.Name, expected_datatypes[0])
+    age_valid = isinstance(p.Age, expected_datatypes[1])
+
+    if name_valid and age_valid:
+        print(f'Name: {p.Name}, Age: {p.Age}')
     else:
-        print(f"Warning! Invalid data type. Expected {expected_datatypes}, Actual Name: {type(person.Name)}, Age: {type(person.Age)}")
+        name_expected = expected_datatypes[0]
+        name_actual = p.Name
+        age_expected = expected_datatypes[1]
+        age_actual = p.Age
+
+        print(f'Warning! Invalid data type. Expected Name: {name_expected}, Actual Name: {name_actual}, Expected Age: {age_expected}, Actual Age: {age_actual}')
--- HW6_2.py
+++ HW6_2.py
@@ -1,10 +1,15 @@
 input_string = 'hello 1 one two three 15 world'
 words_in_a_row = input_string.split()
 
+found_three_words = False
+
 for i in range(len(words_in_a_row) - 2):
-    if words_in_a_row[i].isalnum() and words_in_a_row[i + 1].isalnum() and words_in_a_row[i + 2].isalnum():
-        print('The string contains three words in a row')
+    if words_in_a_row[i].isalpha() and words_in_a_row[i + 1].isalpha() and words_in_a_row[i + 2].isalpha():
+        found_three_words = True
         break
-    else:
-        print('The string does not contain three words in a row')
+
+if found_three_words:
+    print('The string contains three words in a row')
+else:
+    print('The string does not contain three words in a row')
         
